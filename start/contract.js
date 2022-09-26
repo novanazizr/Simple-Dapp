980a37f08d927f6a464892024e836148b959c01d
Novan Aziz Ramadhan <naramadhan77@gmail.com>
2022-09-26 15:21:10 +0700

    Update contract.js

--- start/contract.js
+++ start/contract.js
@@ -1,3 +1,16 @@
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
+
+ethereumButton.addEventListener('click', () => {
+  getAccount();
+});
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}
+
 const initialize = () => {
   //Basic Actions Section
   const onboardButton = document.getElementById('connectButton');
